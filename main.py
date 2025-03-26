e0d184ef72d7639b0ea81cccaf911d1f01a70057
vidyasagar0405 <vidyasagar0405@gmail.com>
2025-03-26 16:34:19 +0530

    L3: FPS

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
 
         for event in pygame.event.get():
@@ -17,6 +20,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
